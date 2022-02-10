2620b87ac9928e447cdcae6501b9219c20856c17
Marcos Rampaso <49240709+Mazner@users.noreply.github.com>
2022-02-10 19:51:11 -0300

    Add files via upload

---
+++ leapyear.c
@@ -0,0 +1,22 @@
+/*this algorith calculates if the year is a leap year*/
+
+#include <stdio.h>
+
+int main(){
+    int year;
+    printf("Tell me the year, and i'll tell if it's a leap year or not > ");
+    scanf("%d",&year);
+    printf("\n");
+
+    if(year % 100 == 0 && year % 400 ==0){
+        printf("%d is a leap year!",year);
+        
+    }else if (year % 4 == 0 && year % 100 != 0){
+        printf("%d is a leap year!",year);
+    }else{
+        printf("%d is not a leap year!",year);
+    }
+    
+
+
+}
